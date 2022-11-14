15f8e1fe02514d6f19635555cf7d66ee9f2adc71
tmcorbin96 <115915843+tmcorbin96@users.noreply.github.com>
2022-11-14 10:51:35 -0500

    still bad

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -39,7 +39,10 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+    for (int counter = 0; counter < personArray.length; counter++) {
+        Person person = personArray[counter];
+        result += person.toString();
+    }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -56,14 +59,17 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+StringBuilder sb = new StringBuilder();
+for (Person person:personArray) {
+    sb.append(person.toString());
+}
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
                 // append `stringRepresentation` to `result` variable
             // end loop
 
-        return result;
+        return sb.toString();
     }
 
 
